f9876041ffa8ab6a0f7e550089d4881639845c9b
nandini1214 <ec20.nandinidewra@svceindore.ac.in>
2025-11-07 14:18:10 +0530

    updated the backend

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,9 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv 
 
-DATABASE_URL = "sqlite:///./slotswagger.db"
-engine =  create_engine(DATABASE_URL, connect_args={"check_same_thread" : False})
+load_dotenv()
+
+DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("❌ DATABASE_URL is not set in .env file")
+engine =  create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
